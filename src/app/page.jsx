6beac11f98cda8c42f280423436f36994096290e
Sabarinath P <sabarinathp@Sabarinaths-MacBook-Air.local>
2025-01-09 01:52:12 +0530

    missing import removed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,73 +4,70 @@ import IndustriesSection from "@/components/IndustriesSection";
 import LogoGrid from "@/components/LogoGrid";
 import Mission from "@/components/Mission";
 import MissionSection from "@/components/MissionSection ";
-import Nav from "@/components/Nav";
 import Services from "@/components/Services";
-import ServicesSection from "@/components/Digital";
-import Image from "next/image";
+
 import ScrollRevealComponent from "../components/ScrollReveal";
-import MouseSpotlight from "@/components/MouseSpotlight";
-import Demo from "@/components/Demo";
+
 import EnhancedCarousel from "@/components/Parallax";
 import ContinuousCarousel from "@/components/InfiniteCorousel";
 import TestimonialSlider from "@/components/Testimonial";
 export default function Home() {
   return (
     <div className=" " style={{ scrollBehavior: "smooth" }}>
-       
-     
-       <ScrollRevealComponent />
+
+
+      <ScrollRevealComponent />
       <div className="snap-center ">
-      <Hero/>
+        <Hero />
       </div>
 
       <div className="snap-center mx-4  reveal ">
         <div className="w-full ">
-          <EnhancedCarousel/>
+          <EnhancedCarousel />
         </div>
       </div>
 
       <div className="snap-center reveal">
-      <MissionSection/>
+        <MissionSection />
       </div>
 
       <div className="snap-center reveal">
-        <Mission/>
+        <Mission />
 
       </div>
 
       <div className="snap-center ">
-        <IndustriesSection/>
+        <IndustriesSection />
 
       </div>
 
       <div className="snap-center ">
-      <ContinuousCarousel/>
-      
-      </div>
-
-      <div className="snap-center reveal">
-        <Blog/>
+        <ContinuousCarousel />
 
       </div>
 
       <div className="snap-center reveal">
-        <Services/>
+        <Blog />
 
       </div>
 
       <div className="snap-center reveal">
-        <TestimonialSlider/>
+        <Services />
+
+      </div>
+
+      <div className="snap-center reveal">
+        <TestimonialSlider />
 
       </div>
 
       <div className="snap-center">
-        <LogoGrid/>
+        <LogoGrid />
 
       </div>
 
-     
-   
-   </div>
+
+
+    </div>
   );
 }
